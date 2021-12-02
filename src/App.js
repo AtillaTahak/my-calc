860869ce2e6b90b05366535e83d3f0752ef6e5a4
Atilla Taha Kördüğüm <atillatahaa@gmail.com>
2021-12-02 18:59:15 +0300

    deleted unnecessary file and fixed size problem

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
+import React from 'react';
 import './App.css';
 import Calculator from './components/Calculator';
 
-function App() {
-  return (
-    <Calculator />
-  );
+class App extends React.Component() {
+  render() {
+    return (
+      <Calculator />
+    );
+  }
 }
 
 export default App;
