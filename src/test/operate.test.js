9c12ca23f9893792c38c02442b78698d460e8a28
Atilla Taha Kördüğüm <atillatahaa@gmail.com>
2021-12-13 11:51:11 +0300

    added netfily

--- src/test/operate.test.js
+++ src/test/operate.test.js
@@ -1,7 +1,6 @@
 import operate from '../logic/operate';
-import '@testing-library/jest-dom'
+import '@testing-library/jest-dom';
 
-
-it('test',()=>{
-    expect(operate(1,2,"+").toBeDefined());
-})
+it('test', () => {
+  expect(operate(1, 2, '+').toBeDefined());
+});
